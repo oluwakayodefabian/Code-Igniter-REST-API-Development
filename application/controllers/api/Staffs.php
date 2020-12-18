686d31d873611e7ac1f3bad7f481cd24841fc393
Owotoke Oluwakayode <oluwakayodefabian@gmail.com>
2020-12-18 11:09:36 +0100

    changes made to Staffs.php and Students.php

--- application/controllers/api/Staffs.php
+++ application/controllers/api/Staffs.php
@@ -2,22 +2,22 @@
 
 date_default_timezone_set('Africa/Lagos');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require(APPPATH . 'libraries/REST_Controller.php');
 
-class Staffs extends REST_Controller 
+class Staffs extends REST_Controller
 {
     public $data = [];
     public $staff_id = NULL;
-    public function __construct() 
+    public function __construct()
     {
         parent::__construct();
         $this->load->model(['api/staff_model']);
         $this->load->library(['form_validation']);
     }
-    public function index_post() 
-    { 
+    public function index_post()
+    {
         $this->data = $this->security->xss_clean($this->input->post());
         $this->form_validation->set_data($this->data);
         $this->form_validation->set_rules('first_name', 'First Name', 'required');
@@ -26,17 +26,14 @@ class Staffs extends REST_Controller
         $this->form_validation->set_rules('phone_no', 'Phone Number', 'required|numeric');
         $this->form_validation->set_rules('role', 'Role', 'required');
         $this->form_validation->set_rules('course_being_taught', 'Course', 'required');
-    
-        if ($this->form_validation->run() === FALSE) 
-        {
-         $this->set_response([
-           'status'     => FALSE,
-           'error'      => $this->form_validation->error_array(),
-           'message'    => validation_errors()
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->set_response([
+                'status'     => FALSE,
+                'error'      => $this->form_validation->error_array(),
+                'message'    => validation_errors()
             ], REST_Controller::HTTP_NOT_FOUND);
-          } 
-          else 
-          {
+        } else {
             $this->data = [
                 'first_name'        => $this->input->post('first_name'),
                 'surname'           => $this->input->post('surname'),
@@ -45,44 +42,32 @@ class Staffs extends REST_Controller
                 'role'              => $this->input->post('role'),
                 'course_being_taught' => $this->input->post('course_being_taught')
             ];
-            if ($this->staff_model->insert_staff($this->data)) 
-            {
+            if ($this->staff_model->insert_staff($this->data)) {
                 $this->set_response(['status' => 1, 'Staff Added Successfully'], REST_Controller::HTTP_OK);
+            } else {
+                $this->set_response(['status' => FALSE, 'Operation failed'], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
             }
-            else
-            {
-                $this->set_response(['status' => FALSE, 'Operation failed'], REST_Controller::HTTP_INTERNAL_SERVER_ERROR); 
-            }
-          }
+        }
     }
     public function index_get()
     {
         $this->data = $this->staff_model->get_all_staffs();
-        if (!$this->data) 
-        {
+        if (!$this->data) {
             throw new Exception("Error Processing Request");
-        }
-        else
-        {
-            if (count($this->data) > 0) 
-            {
+        } else {
+            if (count($this->data) > 0) {
                 $this->set_response(['status' => 1, 'Staff\'s data retrieved Successfully', 'data' => $this->data], REST_Controller::HTTP_OK);
-            }
-            else
-            {
+            } else {
                 $this->set_response(['status' => FALSE, 'Operation failed'], REST_Controller::HTTP_NOT_FOUND);
             }
-        } 
+        }
     }
     public function index_put()
     {
         $this->data = json_decode(file_get_contents('php://input'));
-        if (!isset($this->data->id) && !isset($this->data->first_name) && !isset($this->data->surname) && !isset($this->data->email) && !isset($this->data->phone_no) && !isset($this->data->role) && !isset($this->data->course_being_taught)) 
-        {
+        if (!isset($this->data->id) && !isset($this->data->first_name) && !isset($this->data->surname) && !isset($this->data->email) && !isset($this->data->phone_no) && !isset($this->data->role) && !isset($this->data->course_being_taught)) {
             $this->set_response(['status' => FALSE, "message" => 'All fields are required'], REST_Controller::HTTP_NOT_FOUND);
-        }
-        else
-        {
+        } else {
             $this->staff_id = $this->data->id;
             $this->data = [
                 'first_name' => $this->data->first_name,
@@ -92,13 +77,10 @@ class Staffs extends REST_Controller
                 'role'       => $this->data->role,
                 'course_being_taught' => $this->data->course_being_taught,
             ];
-            if ($this->staff_model->update_staff_info($this->staff_id, $this->data))
-            {
+            if ($this->staff_model->update_staff_info($this->staff_id, $this->data)) {
                 $this->set_response(['status' => 1, 'message' => 'Staff Info Updated Successfully'], REST_Controller::HTTP_OK);
-            }
-            else
-            {
-                $this->response(array('status' => 0, 'message' => 'Registration Failed'), REST_Controller::HTTP_INTERNAL_SERVER_ERROR); 
+            } else {
+                $this->response(array('status' => 0, 'message' => 'Registration Failed'), REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
             }
         }
     }
@@ -106,12 +88,9 @@ class Staffs extends REST_Controller
     {
         $this->data = json_decode(file_get_contents('php://input'));
         $this->staff_id = $this->security->xss_clean($this->data->id);
-        if ($this->staff_model->delete_staff($this->staff_id))
-        {
+        if ($this->staff_model->delete_staff($this->staff_id)) {
             $this->set_response(['status' => 1, 'message' => 'Staff Info Deleted Successfully'], parent::HTTP_OK);
-        }
-        else
-        {
+        } else {
             $this->set_response(array('status' => 0, 'message' => 'Failed to delete staff'), REST_Controller::HTTP_NOT_FOUND);
         }
     }
